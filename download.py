10da7cdfcb9d31542f3a789062c0a8802b54424a
Roland Kwitt <Roland.Kwitt@sbg.ac.at>
2024-06-21 10:23:43 +0200

    UPD: download.py

--- download.py
+++ download.py
@@ -1,6 +1,13 @@
+"""Download precomputed simulation data, persistence diagrams and vectorizations.
+
+szeng, fgraf, muray, shuber, 2024
+"""
+
 import os
 import gdown
 import argparse
+from pathlib import Path
+
 
 DOWNLOAD_LINKS = {
     'dorsogna-1k': {
@@ -15,7 +22,8 @@ DOWNLOAD_LINKS = {
         'simu_10k.pt': '1pevrjzcL1lLDeDgFh1uNOXlW1V-Qb6fo',
         'prms_1k.pt': '1crOSVmiMkoHyWrHH7RqIjITUN1q506s_',
         'prms_1k_norm.pt': '1kFR54X-8IP-Ca2ljd0oCyx7MfvSWi1p2',
-        'dgms_1k_vr_h0h1.pt': '1OKfdKVrmzAGcMacycuBpfcHriiEoeVRo',         
+        'dgms_1k_vr_h0h1.pt': '1OKfdKVrmzAGcMacycuBpfcHriiEoeVRo',    
+        'vecs_20_0.005.pt': '1UaMhNBeFtqlX12WdAp_oXuPpTQq8nHbs'
     }
 }
 
@@ -26,7 +34,7 @@ def setup_cmdline_parsing():
     group0.add_argument("--dataset", 
                         type=str, 
                         default='dorsogna-1k',
-                        choices=['dorsogna-1k'])
+                        choices=['dorsogna-1k', 'volex-10k'])
     group0.add_argument("--destination", type=str, default="/tmp/")
     return generic_parser
 
@@ -36,13 +44,17 @@ def main():
     args = parser.parse_args()
     print(args)
     
+    Path(args.destination).mkdir(parents=True, exist_ok=True)
+    
     links = DOWNLOAD_LINKS[args.dataset]
     
     for k,v in links.items():
         url = 'https://drive.google.com/uc?id={}'.format(v)
         output = os.path.join(args.destination,k)
-        gdown.download(url, output, quiet=False)
-
+        if not os.path.exists(output):
+            gdown.download(url, output, quiet=False)
+        else:
+            print(f'{output} already exists!')
 
 if __name__ == "__main__":
     main()

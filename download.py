2838e3681349e35405ba50042b5733bb713bb7bf
Martin Uray <martin.uray@fh-salzburg.ac.at>
2024-06-27 15:34:12 +0200

    [UPD] formatting to download

--- download.py
+++ download.py
@@ -16,8 +16,8 @@ DOWNLOAD_LINKS = {
         'prms_1k_norm.pt': '1ZiMM2ALYkM7DnXaTHWyr3qUjcQrzMj8O',
         'dgms_1k_vr_h0h1.pt': '15J3XbWjbG3WZQgH-2UbxjUTKl5rDpLmx',
         'vecs_20_0.005.pt': '1QKzw1TjWlJ3sjX1Iy7akcF5iwxAleR3n',
-        'dgms_1k_vr_h0h1.pt': '15J3XbWjbG3WZQgH-2UbxjUTKl5rDpLmx'
     },
+    
     'volex-10k': {
         'simu_10k.pt': '1pevrjzcL1lLDeDgFh1uNOXlW1V-Qb6fo',
         'prms_1k.pt': '1crOSVmiMkoHyWrHH7RqIjITUN1q506s_',
@@ -38,6 +38,7 @@ def setup_cmdline_parsing():
     group0.add_argument("--destination", type=str, default="/tmp/")
     return generic_parser
 
+
 def main():
     
     parser = setup_cmdline_parsing()
@@ -48,15 +49,14 @@ def main():
     
     links = DOWNLOAD_LINKS[args.dataset]
     
-    for k,v in links.items():
+    for k, v in links.items():
         url = 'https://drive.google.com/uc?id={}'.format(v)
-        output = os.path.join(args.destination,k)
+        output = os.path.join(args.destination, k)
         if not os.path.exists(output):
             gdown.download(url, output, quiet=False)
         else:
             print(f'{output} already exists!')
 
+
 if __name__ == "__main__":
     main()
-
-
